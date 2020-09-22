631dc9b7a18bb175a9fcda4197006592a772add2
gefloduDeveloper <germna.florezd@gmail.com>
2020-09-21 20:54:55 -0500

    Initial user interface in windows forms

---
+++ SkiResortTrack/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SkiResortTrack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMapFile = new System.Windows.Forms.Label();
+            this.btnMapFileSelector = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTaskDescription = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMapFile
+            // 
+            this.lblMapFile.AutoSize = true;
+            this.lblMapFile.Location = new System.Drawing.Point(13, 13);
+            this.lblMapFile.Name = "lblMapFile";
+            this.lblMapFile.Size = new System.Drawing.Size(97, 13);
+            this.lblMapFile.TabIndex = 0;
+            this.lblMapFile.Text = " Select a map file...";
+            // 
+            // btnMapFileSelector
+            // 
+            this.btnMapFileSelector.Location = new System.Drawing.Point(270, 8);
+            this.btnMapFileSelector.Name = "btnMapFileSelector";
+            this.btnMapFileSelector.Size = new System.Drawing.Size(28, 23);
+            this.btnMapFileSelector.TabIndex = 1;
+            this.btnMapFileSelector.Text = "...";
+            this.btnMapFileSelector.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Task:";
+            // 
+            // lblTaskDescription
+            // 
+            this.lblTaskDescription.AutoSize = true;
+            this.lblTaskDescription.Location = new System.Drawing.Point(190, 45);
+            this.lblTaskDescription.Name = "lblTaskDescription";
+            this.lblTaskDescription.Size = new System.Drawing.Size(0, 13);
+            this.lblTaskDescription.TabIndex = 3;
+            this.lblTaskDescription.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Progress";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 104);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(282, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(331, 37);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(340, 90);
+            this.txtResult.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(593, 134);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 167);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTaskDescription);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMapFileSelector);
+            this.Controls.Add(this.lblMapFile);
+            this.Name = "Form1";
+            this.Text = "Kitzbühel ski resort track seeker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMapFile;
+        private System.Windows.Forms.Button btnMapFileSelector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTaskDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
